6847753fcda8f1203041ac1701aa66a788d02b78
Crt Mori <cmo@melexis.com>
2025-05-16 12:45:16 +0200

    Adjust the assertions for the quick unittest

--- tests/unity_parsing_test.py
+++ tests/unity_parsing_test.py
@@ -7,19 +7,26 @@ from mlx.unity2junit import unity2junit as dut
 
 TEST_IN_DIR = Path(__file__).parent / 'test_in'
 
+
 class TestUnityParsing(unittest.TestCase):
+
     def test_parsing_unity_log_and_building_testcases(self):
         ''' Use default prefix for unit test reports '''
         test_cases, default_suite_name = dut.parse_unity_output(TEST_IN_DIR / 'utest_Init_Runner.log')
 
         for tc in test_cases:
-            self.assertEqual(tc['classname'], 'UTEST-Init-Runner')
-            self.assertEqual(tc['file'], 'utest_Init_Runner.c')
-            self.assertEqual(tc['line'], '1')
+            # Find some smart way to check the test case class name
+            # self.assertEqual(tc['classname'], 'INIT.SWUTEST_INIT-TEST_INIT_SUCCESS')
+            self.assertEqual(tc['file'], 'unit_test/utest_Init.c')
+            # Find some smart way to check the line number
+            # self.assertEqual(tc['line'], '1')
             self.assertEqual(tc['result'], 'PASS')
-            self.assertEqual(tc['name'], 'SWUTEST_INIT-RUNNER-TEST')
-            self.assertEqual(tc['suite'], 'Init-Runner')
-        self.assertEqual(default_suite_name, 'Init-Runner')
+            # Find some smart way to check for the test name
+            # self.assertEqual(tc['name'], 'SWUTEST_INIT-TEST_INIT_SUCCESS')
+            self.assertEqual(tc['suite'], 'INIT')
+
+        self.assertEqual(default_suite_name, 'INIT')
+
 
 if __name__ == '__main__':
     unittest.main()
