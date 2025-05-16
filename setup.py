b2fb45681377f8a3847a8d079ca0c2490742a698
Crt Mori <cmo@melexis.com>
2025-05-16 12:44:48 +0200

    Move packaging stuff from setup to pyproject.toml
    That is the new way of working

--- setup.py
+++ setup.py
@@ -6,30 +6,7 @@ requires = [
 setup(
     name='mlx.unity2junit',
     url='https://github.com/melexis/unity2junit',
-    license='Apache License Version 2.0',
-    author='Anton Manzhelii',
-    author_email='amz@melexis.com',
-    description='Python script for converting Unity test framework log file to xUnit/JUnit XML',
-    long_description=open("README.rst").read(),
-    long_description_content_type='text/x-rst',
     zip_safe=False,
-    classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Environment :: Console',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: Apache Software License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-        'Programming Language :: Python :: 3.11',
-        'Programming Language :: Python :: 3.12',
-        'Topic :: Documentation',
-        'Topic :: Documentation :: Sphinx',
-        'Topic :: Utilities',
-    ],
     platforms='any',
     packages=find_namespace_packages(where=".", exclude=("doc.*", "doc", "tests.*", "tests", "build*")),
     package_dir={"": "."},
@@ -37,12 +14,4 @@ setup(
     },
     include_package_data=True,
     install_requires=requires,
-    python_requires='>=3.8',
-    keywords=['xUnit', 'JUnit', 'XML', 'Unity test framework', 'testing'],
-    entry_points={
-        'console_scripts': [
-            'mlx.unity2junit = mlx.unity2junit.unity2junit:main',
-            'unity2junit = mlx.unity2junit.unity2junit:main',
-        ]
-    },
 )
